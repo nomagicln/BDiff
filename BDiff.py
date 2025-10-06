44575c17ea437a3bf1bddfde35d469e876c2e172
Roadfar <839377654@qq.com>
2025-10-06 14:11:59 +0800

    update comments

--- BDiff.py
+++ BDiff.py
@@ -1,4 +1,4 @@
-```
+'''
 Copyright (c) [2025] [Lu YAO]
 BDiff is licensed under Mulan PubL v2.
 You can use this software according to the terms and conditions of the Mulan PubL v2.
@@ -8,7 +8,7 @@ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PubL v2 for more details.
-```
+'''
                      
 import copy
 import os
