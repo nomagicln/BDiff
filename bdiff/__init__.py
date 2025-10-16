fbb14151377201dfeee9c402abda4275eb0c5c97
Roadfar <839377654@qq.com>
2025-10-16 18:18:17 +0800

    完善代码并添加测试代码及案例

--- bdiff/__init__.py
+++ bdiff/__init__.py
@@ -10,9 +10,9 @@
 
 """BDiff is a block-aware and accurate text-based difference tool."""
 
-__all__ = ["BDiff", "BDiffFile"]
+__all__ = ["bdiff"]
 
 __version__ = "0.1.0"
 __author__ = "Lu Yao <839377654@qq.com>"
 
-from .bdiff import BDiff, BDiffFile
+from .bdiff import bdiff
---
+++ bdiff/demo.py
@@ -0,0 +1,5 @@
+import bdiff
+import pprint
+
+if __name__ == "__main__":
+    print(bdiff.bdiff(r"../diff-cases/left_files/4-move-copy-before.xml", r"../diff-cases/right_files/4-move-copy-after.xml"))
